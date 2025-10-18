8b4430700166382be50822c26cc595ba6b7501a9
Dan5326 <127957242+D53261@users.noreply.github.com>
2025-10-17 22:01:30 -0300

    Add gender and age acceptance check program
    Implements a program to read gender and age, and determine acceptance based on criteria.

---
+++ atividade6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Ler nome, sexo e idade. Se sexo for feminino e idade menor que 25, imprime o nome da pessoa e a palavra “ACEITA”, caso contrario imprimir “N  ́ AO ACEITA”.
+int main (){
+    int idade;
+    char genero;
+
+    printf("insira seu genero:\nH para homem\nM para mulher \n");
+    scanf("%s", &genero);
+    if (genero == "H" || genero == "h"){
+        printf("NAO ACEITO");
+    } else {
+        printf("insira sua idade: \n");
+        scanf("%d", &idade);
+        if(idade<25){
+            printf("NAO ACEITO");
+        } else {
+            printf("ACEITA");
+        }
+    }
+    return 0;
+}
